93c6853aab60e633acc6b6dd8887dd079cd79f71
Max Quebral <maxquebral@gmail.com>
2020-03-23 23:12:32 +0800

    Added faker library

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -2,9 +2,27 @@
 
 namespace Tests;
 
+use Faker\Factory;
+use Faker\Generator;
 use PHPUnit\Framework\TestCase;
 
 abstract class AbstractTestCase extends TestCase
 {
-    //...
+
+    /**
+     * @var \Faker\Generator
+     */
+    private ?\Faker\Generator $faker = null;
+
+    /**
+     * @return \Faker\Generator
+     */
+    protected function getFaker(): Generator
+    {
+        if ($this->faker !== null) {
+            return $this->faker;
+        }
+
+        return $this->faker = Factory::create();
+    }
 }
